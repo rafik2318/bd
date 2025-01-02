0d6a026fdeffc703013e7716df07ab7499014c92
rafik2318 <150452391+rafik2318@users.noreply.github.com>
2025-01-02 10:08:03 +0400

    Update app.py
    добавил базовый класс для моделей
    создал сессию и функцию для подключения к ней

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ db_url = "postgresql://rafik:1sPmKldQwj8mx@83.149.198.142/rafik_db"
 
 engine = create_engine(db_url)
 
-with  engine.connect() as conn:
-	res = conn.execute(text("SELECT VERSION()"))
-	print(f"{res=}")
+Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
