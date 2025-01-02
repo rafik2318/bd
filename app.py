6078326dfb8d0ff505f69d6466269e25e7c21241
rafik2318 <150452391+rafik2318@users.noreply.github.com>
2025-01-02 10:15:34 +0400

    Update app.py
    базовый класс уже был, испортировал его из моделс

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from models impord bd
 
 db_url = "postgresql://rafik:1sPmKldQwj8mx@83.149.198.142/rafik_db"
 
 engine = create_engine(db_url)
-
-Base = declarative_base()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
