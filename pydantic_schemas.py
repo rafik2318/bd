376ce9065ce442c038df574e6f6883a9a9cc7744
rafik2318 <150452391+rafik2318@users.noreply.github.com>
2025-01-02 10:47:26 +0400

    Update pydantic_schemas.py
    добавил функции update для всех моделей

--- pydantic_schemas.py
+++ pydantic_schemas.py
@@ -1,5 +1,6 @@
 from pedantic import BaseModel
 from datetime import Date
+from typing import Optional
 
 class BookBase(BaseModel):
   author: str
@@ -7,30 +8,37 @@ class BookBase(BaseModel):
   publisher: str
   topic: str
 
-class BookCreate(BookBase):
-  pass
-
-class Book(BookBase):
-  id: int
-
   class Config:
     orm_mode = True
 
+
+class BookCreate(BookBase):
+  pass
+
+class BookUpdate(BookBase):
+  author: Optional[str] = None
+  name: Optional[str] = None
+  publisher: Optional[str] = None
+  topic: Optional[str] = None
+
+  
 class LoanBase(BaseModel):
   take_date: date
   return_date: date
   where_returned: date
 
+  class Config:
+    orm_mode = True
+
 class LoanCreate(LoanBase):
   pass
 
-class Loan(LoanBase):
-  id: int
-  book_id: int
-  reader_id: int
+class LoanUpdate(LoanBase):
+  take_date: Optional[date] = None
+  return_date: Optional[date] = None
+  where_returned: Optional[date] = None
 
-  class Config:
-    orm_mode = True
+  
 
 class ReaderBase(BaseModel):
   full_name: str
@@ -39,11 +47,17 @@ class ReaderBase(BaseModel):
   passport_number: str
   departure_mark: str
 
+  class Config:
+        orm_mode = True
+
 class ReaderCreate(ReaderBase):
   pass
 
-class Reader(ReaderBase):
-  id: int
+class ReaderUpdate(ReaderBase):
+  full_name: Optional[str] = None
+  address: Optional[str] = None
+  phone_number: Optional[str] = None
+  passport_number: Optional[str] = None
+  departure_mark: Optional[str] = None
 
-  class Config:
-    orm_mode = True
+  
