ddf74a6dab1cc4fdaf0c53ae970f0dd2f71313d0
rafik2318 <150452391+rafik2318@users.noreply.github.com>
2025-01-09 11:32:40 +0400

    Update app.py
    исправил ошибку

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-from models impord bd
+from models import bd
 
 db_url = "postgresql://rafik:1sPmKldQwj8mx@83.149.198.142/rafik_db"
 
